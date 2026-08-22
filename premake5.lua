0b612a965885d306fc2314407b00370d8eea86ac
DerDoktorX <>
2026-08-22 21:33:52 +0200

    premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,172 @@
+project "assimp"
+
+	if BuildProjectConf == "Static" or BuildProjectConf == "StaticLib" then
+		staticruntime "on"
+	end
+	if BuildProjectConf == "Static2" or BuildProjectConf == "Static2Lib" then
+		staticruntime "off"
+	end
+	if BuildProjectConf == "Dynamic" then
+		staticruntime "off"
+	end
+
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/**.h",
+		"include/**.hpp",
+		"include/**.inl",
+
+		-- Kern-Infrastruktur (Importer/Exporter-Framework, Szene, Postprocessing, Material, Geometrie)
+		"code/Common/**.cpp",
+		"code/Common/**.h",
+		"code/CApi/**.cpp",
+		"code/CApi/**.h",
+		"code/Material/**.cpp",
+		"code/Material/**.h",
+		"code/PostProcessing/**.cpp",
+		"code/PostProcessing/**.h",
+		"code/Geometry/**.cpp",
+		"code/Geometry/**.h",
+
+		-- Nur die aktuell benoetigten Formate
+		"code/AssetLib/FBX/**.cpp",
+		"code/AssetLib/FBX/**.h",
+		"code/AssetLib/glTF/**.cpp",
+		"code/AssetLib/glTF/**.h",
+		"code/AssetLib/glTF/**.inl",
+		"code/AssetLib/glTF2/**.cpp",
+		"code/AssetLib/glTF2/**.h",
+		"code/AssetLib/glTF2/**.inl",
+		"code/AssetLib/Obj/**.cpp",
+		"code/AssetLib/Obj/**.h",
+
+		-- zlib und unzip werden von Common/ZipArchiveIOSystem.cpp + FBX (komprimierte Streams)
+		-- IMMER unbedingt gebraucht, unabhaengig vom Format
+		"contrib/zlib/*.c",
+		"contrib/zlib/*.h",
+		"contrib/unzip/*.c",
+		"contrib/unzip/*.h",
+
+		-- generischer XML-Parser (kleine, guenstige Absicherung falls Common intern genutzt)
+		"contrib/pugixml/src/pugixml.cpp",
+		"contrib/pugixml/src/*.hpp",
+	}
+
+	includedirs
+	{
+		".",
+		"code",
+		"include",
+
+		"contrib",                     -- loest "stb/stb_image.h" auf (Common/StbCommon.h)
+		"contrib/zlib",
+		"contrib/unzip",
+		"contrib/pugixml/src",
+		"contrib/rapidjson/include",   -- glTF/glTF2 JSON-Parsing
+		"contrib/utf8cpp/source",      -- loest "utf8.h" auf (u.a. Common/BaseImporter.cpp)
+	}
+
+	defines
+	{
+        "RAPIDJSON_HAS_STDSTRING",
+        "RAPIDJSON_NOMEMBERITERATORCLASS",
+        
+		-- Importer, die wir NICHT brauchen (nur GLTF/GLTF2/FBX/OBJ bleiben aktiv)
+		"ASSIMP_BUILD_NO_USD_IMPORTER",
+		"ASSIMP_BUILD_NO_X_IMPORTER",
+		"ASSIMP_BUILD_NO_AMF_IMPORTER",
+		"ASSIMP_BUILD_NO_3DS_IMPORTER",
+		"ASSIMP_BUILD_NO_MD3_IMPORTER",
+		"ASSIMP_BUILD_NO_MDL_IMPORTER",
+		"ASSIMP_BUILD_NO_MD2_IMPORTER",
+		"ASSIMP_BUILD_NO_PLY_IMPORTER",
+		"ASSIMP_BUILD_NO_ASE_IMPORTER",
+		"ASSIMP_BUILD_NO_HMP_IMPORTER",
+		"ASSIMP_BUILD_NO_SMD_IMPORTER",
+		"ASSIMP_BUILD_NO_MDC_IMPORTER",
+		"ASSIMP_BUILD_NO_MD5_IMPORTER",
+		"ASSIMP_BUILD_NO_STL_IMPORTER",
+		"ASSIMP_BUILD_NO_LWO_IMPORTER",
+		"ASSIMP_BUILD_NO_DXF_IMPORTER",
+		"ASSIMP_BUILD_NO_NFF_IMPORTER",
+		"ASSIMP_BUILD_NO_RAW_IMPORTER",
+		"ASSIMP_BUILD_NO_SIB_IMPORTER",
+		"ASSIMP_BUILD_NO_OFF_IMPORTER",
+		"ASSIMP_BUILD_NO_AC_IMPORTER",
+		"ASSIMP_BUILD_NO_BVH_IMPORTER",
+		"ASSIMP_BUILD_NO_IRRMESH_IMPORTER",
+		"ASSIMP_BUILD_NO_IRR_IMPORTER",
+		"ASSIMP_BUILD_NO_Q3D_IMPORTER",
+		"ASSIMP_BUILD_NO_B3D_IMPORTER",
+		"ASSIMP_BUILD_NO_COLLADA_IMPORTER",
+		"ASSIMP_BUILD_NO_TERRAGEN_IMPORTER",
+		"ASSIMP_BUILD_NO_CSM_IMPORTER",
+		"ASSIMP_BUILD_NO_3D_IMPORTER",
+		"ASSIMP_BUILD_NO_LWS_IMPORTER",
+		"ASSIMP_BUILD_NO_OGRE_IMPORTER",
+		"ASSIMP_BUILD_NO_OPENGEX_IMPORTER",
+		"ASSIMP_BUILD_NO_MS3D_IMPORTER",
+		"ASSIMP_BUILD_NO_COB_IMPORTER",
+		"ASSIMP_BUILD_NO_BLEND_IMPORTER",
+		"ASSIMP_BUILD_NO_Q3BSP_IMPORTER",
+		"ASSIMP_BUILD_NO_NDO_IMPORTER",
+		"ASSIMP_BUILD_NO_IFC_IMPORTER",
+		"ASSIMP_BUILD_NO_XGL_IMPORTER",
+		"ASSIMP_BUILD_NO_ASSBIN_IMPORTER",
+		"ASSIMP_BUILD_NO_C4D_IMPORTER",
+		"ASSIMP_BUILD_NO_3MF_IMPORTER",
+		"ASSIMP_BUILD_NO_X3D_IMPORTER",
+		"ASSIMP_BUILD_NO_MMD_IMPORTER",
+		"ASSIMP_BUILD_NO_M3D_IMPORTER",
+		"ASSIMP_BUILD_NO_IQM_IMPORTER",
+
+		-- Exporter, deren Quellordner wir gar nicht erst kompilieren
+		-- (sonst: "unresolved external symbol" in Exporter.cpp)
+		"ASSIMP_BUILD_NO_X_EXPORTER",
+		"ASSIMP_BUILD_NO_STEP_EXPORTER",
+		"ASSIMP_BUILD_NO_STL_EXPORTER",
+		"ASSIMP_BUILD_NO_PLY_EXPORTER",
+		"ASSIMP_BUILD_NO_3DS_EXPORTER",
+		"ASSIMP_BUILD_NO_COLLADA_EXPORTER",
+		"ASSIMP_BUILD_NO_ASSBIN_EXPORTER",
+		"ASSIMP_BUILD_NO_ASSXML_EXPORTER",
+		"ASSIMP_BUILD_NO_X3D_EXPORTER",
+		"ASSIMP_BUILD_NO_M3D_EXPORTER",
+		"ASSIMP_BUILD_NO_3MF_EXPORTER",
+		"ASSIMP_BUILD_NO_ASSJSON_EXPORTER",
+		"ASSIMP_BUILD_NO_PBRT_EXPORTER",
+
+        
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+		defines
+		{
+			"WIN32_LEAN_AND_MEAN",
+			"UNICODE",
+			"_UNICODE",
+		}
+
+	filter "toolset:msc*"
+		buildoptions { "/bigobj" }
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
